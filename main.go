c5a4c8c132f822c71da78e0252131d18d4c4862b
The Spudguy <100954270+PurpleSunsetGames@users.noreply.github.com>
2026-06-12 22:25:40 -0400

    broken wrapping

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ const (
 var tempRangeMin = 800.
 var tempRangeMax = 1000.
 var epsilon = 28.
+var wall = false
 
 type group struct {
 	groupParticles []particle
@@ -74,16 +75,30 @@ func (p point) mult(factor float64) point {
 	newPoint.Y = factor * p.Y
 	return *newPoint
 }
+func (p point) addxy(x float64, y float64) point {
+	newPoint := new(point)
+	newPoint.X = p.X + x
+	newPoint.Y = p.Y + y
+	return *newPoint
+}
 
 func (this *particle) applyForce() {
-	if math.Abs(this.pos.X) > screenW/2 {
+	if wall {
+		if math.Abs(this.pos.X) > screenW/2 {
 		this.F.X = this.F.X - wallPotential*(this.pos.X - (screenW/2)*this.pos.X/math.Abs(this.pos.X))
-	}
-	if math.Abs(this.pos.Y) > screenH/2 {
-		this.F.Y = this.F.Y - wallPotential*(this.pos.Y - (screenH/2)*this.pos.Y/math.Abs(this.pos.Y))
+		}
+		if math.Abs(this.pos.Y) > screenH/2 {
+			this.F.Y = this.F.Y - wallPotential*(this.pos.Y - (screenH/2)*this.pos.Y/math.Abs(this.pos.Y))
+		}
 	}
 	this.vel = this.vel.addmult(this.F.addmult(randPoint(1,1), this.temp), tStep).mult(.999)
 	this.pos = this.pos.addmult(this.vel, tStep)
+	if !wall {
+		newP := new(point)
+		newP.X = math.Mod(this.pos.X+screenW/2, screenW) - screenW/2
+		newP.Y = math.Mod(this.pos.Y+screenH/2, screenH) - screenH/2
+		this.pos = *newP
+	}
 	newPoint := new(point)
 	this.F = *newPoint
 	this.temp = rand.Float64()*(tempRangeMax - tempRangeMin) + tempRangeMin
@@ -91,10 +106,33 @@ func (this *particle) applyForce() {
 
 // Leonard-Jones as a place holder
 func (this *particle) interact(other particle) {
-	distTo := math.Sqrt(math.Pow(this.pos.X - other.pos.X, 2) + math.Pow(this.pos.Y - other.pos.Y, 2))
-	vLJ := 2*epsilon*(math.Pow(particleRadius/distTo, 12) - math.Pow(particleRadius/distTo, 6))
-	this.F = this.F.addmult(this.pos.addmult(other.pos, -1), vLJ)
-	other.F = other.F.addmult(other.pos.addmult(this.pos, -1), vLJ)
+	if wall{
+		distTo := math.Sqrt(math.Pow(this.pos.X - other.pos.X, 2) + math.Pow(this.pos.Y - other.pos.Y, 2))
+		if distTo < 3*particleRadius {
+			vLJ := 2*epsilon*(math.Pow(particleRadius/distTo, 12) - math.Pow(particleRadius/distTo, 6))
+			this.F = this.F.addmult(this.pos.addmult(other.pos, -1), vLJ)
+			other.F = other.F.addmult(other.pos.addmult(this.pos, -1), vLJ)
+		}
+	}
+	if !wall {
+		// mirroring for wrapping
+		m1 := other.pos
+		m2 := other.pos.addxy(screenW, 0)
+		m3 := other.pos.addxy(0, screenH)
+		m4 := other.pos.addxy(-screenW, 0)
+		m5 := other.pos.addxy(0, -screenH)
+		mirrors := []point{
+			m1, m2, m3, m4, m5,
+		}
+		for _, v := range mirrors {
+			distTo := math.Sqrt(math.Pow(this.pos.X - v.X, 2) + math.Pow(this.pos.Y - v.Y, 2))
+			if distTo < 3*particleRadius {
+				vLJ := 2*epsilon*(math.Pow(particleRadius/distTo, 12) - math.Pow(particleRadius/distTo, 6))
+				this.F = this.F.addmult(this.pos.addmult(other.pos, -1), vLJ)
+				other.F = other.F.addmult(v.addmult(this.pos, -1), vLJ)
+			}
+		}
+	}
 }
 
 // One worker's computation
